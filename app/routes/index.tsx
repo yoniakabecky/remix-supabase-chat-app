4963ac4b38ef47012a074053053da87f17bd1c46
Yoni <yoniakabecky@gmail.com>
2023-02-12 22:21:56 +0900

    setup spabase

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,12 @@
+import { useLoaderData } from "@remix-run/react";
+import supabase from "utils/supabase";
+
+export const loader = async () => {
+  const { data } = await supabase.from("messages").select();
+  return { messages: data ?? [] };
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const { messages } = useLoaderData<typeof loader>();
+  return <pre>{JSON.stringify(messages, null, 2)}</pre>;
 }
