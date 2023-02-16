fd48ec92c84bcf4809e67490733b85c4d5aa37ea
Yoni <yoniakabecky@gmail.com>
2023-02-16 18:36:41 +0900

    add message input form
    Securely Mutate Supabase Data with Remix Actions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,31 @@
-import type { LoaderArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 
 import Login from "components/login";
 import createServerSupabase from "utils/supabase.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  const response = new Response();
+  const supabase = createServerSupabase({ request, response });
+
+  const { message } = Object.fromEntries(await request.formData());
+  const { error } = await supabase
+    .from("messages")
+    .insert({ content: String(message) });
+
+  if (error) {
+    console.log(error);
+  }
+
+  return json(null, { headers: response.headers });
+};
 
 export const loader = async ({ request }: LoaderArgs) => {
   const response = new Response();
   const supabase = createServerSupabase({ request, response });
   const { data } = await supabase.from("messages").select();
+
   return json({ messages: data ?? [] }, { headers: response.headers });
 };
 
@@ -18,6 +35,11 @@ export default function Index() {
     <>
       <Login />
       <pre>{JSON.stringify(messages, null, 2)}</pre>
+
+      <Form method="post">
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </Form>
     </>
   );
 }
